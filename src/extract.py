058c247efb0d0af9e3f453e8e8c8a6a201d54e85
Jeru Balares <jerupaulbalares@gmail.com>
2023-10-16 20:25:06 -0700

    Added extract_tournament function to extract info of listed finished tournaments from lichess.org/api/tournament: id, fullName, variant, perf, maxRating (if applicable), winner id, winner name, duration

--- src/extract.py
+++ src/extract.py
@@ -1,14 +1,35 @@
 import requests
 import json
 
-response = requests.get('https://lichess.org/api/tournament')
-response_json = json.loads(response.content)
+def extract_tournament():
+    response = requests.get('https://lichess.org/api/tournament')
+    response_json = json.loads(response.content)
+    
+    all_passes = []
+    
+    for response in response_json['finished']:
+    
+        try:
+            current_pass = []
+            current_pass.append(response['id'])
+            current_pass.append(response['fullName'])
+            current_pass.append(response['variant']['key'])
+            current_pass.append(response['perf']['key'])
+            try:
+                current_pass.append(response['maxRating']['rating'])
+            except(KeyError):
+                current_pass.append('null')
+            current_pass.append(response['winner']['id'])
+            current_pass.append(response['winner']['name'])
+            current_pass.append((response['finishesAt'] - response['startsAt']) / 60000)
+            all_passes.append(current_pass)
+        except(Exception):
+            continue
+    
+    return all_passes
 
-tournament_types = []
-
-for headers in response_json:
-        tournament_types.append(headers)
 
 if __name__ == '__main__':
-    print(tournament_types)
-    print(len(response_json['finished']))
+    for passes in extract_tournament():
+        print(passes)
+        print('---------------------------')
